8af5ddd9e51fe23b20f51980c4262e662fab2c34
Azadoz123 <azad.ozkaynak@gmail.com>
2023-12-16 16:06:38 +0300

    Artık Yıl Bulma Programı Düzeltme

--- ArtikYilHesaplama/src/Main.java
+++ ArtikYilHesaplama/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         //Artık yıl bulma
         if(yil % 4 == 0){
-            if( (yil % 100 == 0) && (yil % 400 == 0)){
+            if( (yil % 100 == 0) && (yil % 400 != 0)){
                 System.out.println(yil + " bir artık yıl değildir ! !");
             }else {
                 System.out.println(yil +" bir artık yıldır !");
